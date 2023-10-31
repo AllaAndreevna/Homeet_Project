7f7d8e0a0fc9da2e3eff45cd0802496b440c5122
Alla_Chernova <110739884+5724gnvasya@users.noreply.github.com>
2023-10-31 19:16:16 +0300

    Update script_1.js

--- src/js/script_1.js
+++ src/js/script_1.js
@@ -18,9 +18,9 @@ function myFunction() {
     let users_added_name = document.querySelector("#users_added_name");
     users_added_name.innerHTML = name.value;
 
-    let nom_tel = document.querySelector("#nom_tel");
-    let users_nom_tel = document.querySelector("#users_nom_tel");
-    users_nom_tel.innerHTML = nom_tel.value;
+    // let nom_tel = document.querySelector("#nom_tel");
+    // let users_nom_tel = document.querySelector("#users_nom_tel");
+    // users_nom_tel.innerHTML = nom_tel.value;
 
 
     let osebe = document.querySelector("#osebe");
@@ -34,76 +34,21 @@ function myFunction() {
     if (document.getElementById('devushka').checked) {
         users_added_gender.innerHTML = devushka.value;
     }
-    // document.getElementById("years").textContent = bYear;
+    
+
+
 
 }
 
 //here i'm getting the age
-const months = [31,28,31,30,31,30,31,31,30,31,30,31];
 
-function ageCalculate(){
-    let today = new Date();
-    let inputDate = new Date(document.getElementById("date-input").value);
-    let birthMonth,birthDate,birthYear;
-    let birthDetails = {
-        date:inputDate.getDate(),
-        month:inputDate.getMonth()+1,
-        year:inputDate.getFullYear()
-    }
-    let currentYear = today.getFullYear();
-    let currentMonth = today.getMonth()+1;
-    let currentDate = today.getDate();
-
-    leapChecker(currentYear);
-
-    if(
-        birthDetails.year > currentYear ||
-        ( birthDetails.month > currentMonth && birthDetails.year == currentYear) || 
-        (birthDetails.date > currentDate && birthDetails.month == currentMonth && birthDetails.year == currentYear)
-    ){
-        alert("Not Born Yet");
-        displayResult("-","-","-");
-        return;
-    }
-
-    birthYear = currentYear - birthDetails.year;
-
-    if(currentMonth >= birthDetails.month){
-        birthMonth = currentMonth - birthDetails.month;
-    }
-    else{
-        birthYear--;
-        birthMonth = 12 + currentMonth - birthDetails.month;
-    }
-
-    if(currentDate >= birthDetails.date){
-        birthDate = currentDate - birthDetails.date;
-    }
-    else{
-        birthMonth--;
-        let days = months[currentMonth - 2];
-        birthDate = days + currentDate - birthDetails.date;
-        if(birthMonth < 0){
-            birthMonth = 11;
-            birthYear--;
-        }
-    }
-    displayResult(birthDate,birthMonth,birthYear);
+function submitBday() {
+    let Bdate = document.getElementById('date-input').value;
+    let Bday = +new Date(Bdate);
+    let Q4A = ~~ ((Date.now() - Bday) / (31557600000)) + " лет";
+    let theBday = document.getElementById('users_added_years');
+    theBday.innerHTML = Q4A;
 }
-
-function displayResult(bDate,bMonth,bYear){
-    document.getElementById("years").textContent = bYear;
-}
-
-function leapChecker(year){
-    if(year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)){
-        months[1] = 29;
-    }
-    else{
-        months[1] = 28;
-    }
-}
-
 
 
 
@@ -136,16 +81,17 @@ dropdowns.forEach(dropdown => {
 });
 
 
-//styling fields
-formInputs.forEach(input) {
-    if (input.value == '') {
-        console.log('input empty')
-        input.classList.add('error');
-        let a = "#" + String(input.name) + 'empty'
-        //console.log(a)
-        document.querySelector(a).textContent = 'Обязательное поле'
-    }
-    else{
-        input.classList.remove('error')
-    }
-}
+// //styling fields
+// formInputs.forEach(input) {
+//     if (input.value == '') {
+//         console.log('input empty')
+//         input.classList.add('error');
+//         let a = "#" + String(input.name) + 'empty'
+//         //console.log(a)
+//         document.querySelector(a).textContent = 'Обязательное поле'
+//     }
+//     else{
+//         input.classList.remove('error')
+//     }
+// }
+
